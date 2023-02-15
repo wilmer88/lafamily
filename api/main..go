37bb76fbeb091e604e71429a7577aebd650a81c9
Wilmer Rivera <wilmerbaby@gmail.com>
2023-02-15 09:34:49 -0500

    added project

---
+++ api/main..go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/wilmer88/lafamily/api/controllers"
+
+	// "gorm-test/controllers"
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+)
+
+func main() {
+
+	port := os.Getenv("Port")
+	if port ==""{
+		port = "8080"
+	}
+
+
+	r := setupRouter()
+	_ = r.Run(":"+port)
+	
+}
+
+func setupRouter() *gin.Engine {
+
+	r := gin.Default()
+	config := cors.DefaultConfig()
+	// config.AllowOrigins = []string{"https://lafamily.herokuapp.com/"}
+	config.AllowOrigins = []string{"https://localhost:8080/"}
+
+	r.Use(cors.New(config))
+	r.GET("ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "pong")
+	})
+
+	userRepo := controllers.New()
+	r.POST("/lafamily", userRepo.CreateUser)
+	r.GET("/lafamily", userRepo.GetUsers)
+	r.GET("/lafamily/:id", userRepo.GetUser)
+	r.PUT("/lafamily/:id", userRepo.UpdateUser)
+	r.DELETE("/lafamily/:id", userRepo.DeleteUser)
+
+	return r
+}
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	}
---
+++ api/models/familiaModel.go
@@ -0,0 +1,53 @@
+package models
+
+import (
+	
+	"gorm.io/gorm"
+)
+
+type Fammember struct {
+	gorm.Model
+	Id   int
+	FirstName  string
+	Happiness int
+	UrlStr string
+}
+
+//create a user
+func CreateUser(db *gorm.DB, User *Fammember) (err error) {
+	err = db.Create(User).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//get users
+func GetUsers(db *gorm.DB, User *[]Fammember) (err error) {
+	err = db.Find(User).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//get user by id
+func GetUser(db *gorm.DB, User *Fammember, Id int) (err error) {
+	err = db.Where("Id = ?", Id).First(User).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//update user
+func UpdateUser(db *gorm.DB, User *Fammember) (err error) {
+	db.Save(User)
+	return nil
+}
+
+//delete user
+func DeleteUser(db *gorm.DB, User *Fammember, Id int) (err error) {
+	db.Where("Id = ?", Id).Delete(User)
+	return nil
+}
